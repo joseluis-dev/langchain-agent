a82d1640ddca58d92f89f7dfd8a8a8e641aa67de
Jose Yepez <jl.yg91@gmail.com>
2026-05-24 22:21:01 -0500

    Implement weather tool and enhance agent initialization with API key

--- agent.py
+++ agent.py
@@ -1,6 +1,53 @@
+import os
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain.tools import tool
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
 
 load_dotenv()
 
-model = init_chat_model("gpt-4.1-mini", model_provider="openai")
+model = init_chat_model("gpt-4.1-mini", model_provider="openai", api_key=os.getenv("OPENAI_KEY"))
+
+@tool
+def get_weather(city: str) -> str:
+    """Get the current weather for a given city."""
+    data = {
+        "ambato": "27°C, sunny",
+        "quito": "22°C, cloudy",
+        "guayaquil": "30°C, sunny",
+    }
+    return data.get(city.lower(), "City not found")
+
+checkpointer = InMemorySaver()
+
+agent = create_agent(
+    model=model,
+    tools=[get_weather],
+    system_prompt="Eres un asistente que habla español, y tus respuestas son muy concisas y breves.",
+    checkpointer=checkpointer
+)
+
+config = {"configurable": {"thread_id": "1"}}
+
+agent.invoke(
+    {
+        "messages": [{
+            "role": "user",
+            "content": "Mi nombre es Jose"
+        }]
+    },
+    config=config
+)
+
+result = agent.invoke(
+    {
+        "messages": [{
+            "role": "user",
+            "content": "¿Cuál es mi nombre?"
+        }]
+    },
+    config=config
+)
+
+print(result["messages"][-1].content)
